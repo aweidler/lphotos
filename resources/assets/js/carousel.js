7ceed68c28be01788e14ca6add632b598768edcb
Austin Weidler <aweidler@warwicksd.org>
2016-11-29 20:09:27 -0500

    Finished banner

--- resources/assets/js/carousel.js
+++ resources/assets/js/carousel.js
@@ -1,45 +1,46 @@
-var $test; // only for testing take out later
-var $test2;
-
 $(function(){
-	var scale = 0.5;
-	var animtime = 2;
+	var scale = 0.75; // matches what is in css
+	var animtime = 2; // time for our animation to complete
+	var rotatestd = 30; // max degress for rotation
+	var blurstd = 0; // controls amount of blur on each image
+	var intervalSpeed = 3; // 1 second
+	var canvasAmount = 3 // total amount of images on the canvas at a time
+
+	var pointer = 0;
+	var out = [];
+	var units = '%';
 
 	// cache our items
 	var $content = $('#fsWrapper .content');
 	var $polars = $content.find('> .fspolar img');
-	$test = $($polars[0]);
-	$test2 = $($polars[1]);
 
 	$.fn.findOutPoint = function(){
 		// find a point in this container that we can put our image transform to start
 		var mywidth = $(this).outerWidth();
-		var contentWidth = $content.width();
+		var myheight = $(this).outerHeight();
 		var px = Math.floor(Math.random() * 100) + 1;
 		if(Date.now() % 2 == 0){
 			px *= -1;
 		}
-		var py = -205;
-		return {x: px + '%', y: py + '%'};
+		var py = -100 - (mywidth / myheight * rotatestd) - rotatestd;
+		return {x: px + units, y: py + units};
 	}
 
 	$.fn.findInPoint = function(){
-		var mywidth = $(this).outerWidth();
-		var myheight = $(this).outerHeight();
-		var contentWidth = $content.width();
-		var contentHeight = $content.height();
-		var px = Math.floor(Math.random() * 100) + 1;
-		var py = Math.floor(Math.random() * 100) + 1;
+		var mywidth = $(this).outerWidth(); 
+		var containpercent = (mywidth / $content.outerWidth());
+		var px = Math.floor(Math.random() * (1 / containpercent) * 50 * scale) + 1;
+		var py = Math.floor(Math.random() * Math.abs(100 - 1/scale * 100)) + 1;
 		if(Date.now() % 2 == 0){
 			px *= -1;
 		}
-		return {x: px + '%', y: py + '%'};
+		return {x: px + units, y: py + units};
 	}
 
 
 	$.fn.nextTransform = function(){
 		var point;
-		var rotate = " rotate("+(Math.random() * 60 * (Date.now() % 2 ? 1 : -1))+"deg)";
+		var rotate = " rotate("+(Math.random() * rotatestd * (Date.now() % 2 ? 1 : -1))+"deg)";
 		if(!$(this).data('out')){
 			// we need to move out
 			point = $(this).findOutPoint();
@@ -55,17 +56,62 @@ $(function(){
 		return "translate("+point.x+", "+point.y+")" + rotate;
 	}
 
-	$.fn.fly = function() {
+	$.fn.fly = function(instant) {
 		return this.each(function() {
-			var css = $(this).nextTransform();
+			var css = $(this).nextTransform(); 
+			var anim = (instant) ? 'none' : 'transform ' + animtime + 's';
 			$(this).css({
-				'transition': 'transform ' + animtime + 's',
+				'transition': anim,
 				'-webkit-transform': css,
 				'-moz-transform': css,
 				'-ms-transform': css,
 				'-o-transform': css,
-				'transform': css});
+				'transform': css,
+				'display': 'block'});
 		});
 	};
 
+	// reset all images
+	$polars.fly(true);
+
+	if($polars.length){
+		var ihandle;
+		var curz = 0;
+		(function sanimate(){
+			$cur = false;
+			do{
+				if(pointer > $polars.length - 1){
+					pointer = 0;
+				}
+
+				var cur = $polars.get(pointer);
+				if(cur.complete){
+					$cur = $(cur);
+				}
+				pointer++;
+
+			} while(!$cur);
+
+			if($cur){
+				// update our z-index
+				$cur.css({'z-index': ++curz, 'filter':'none'});
+				$cur.fly();
+
+				if(blurstd){
+					for(var i=0; i<out.length; i++){ 
+						out[i].css('filter', 'blur('+(blurstd * (out.length - i))+'px)');
+					}
+				}
+				if(out.length >= canvasAmount){
+					var $last = out.shift();
+					if($last){
+						$last.fly();
+					}
+				}
+				out.push($cur);
+			}
+
+			ihandle = setTimeout(sanimate, intervalSpeed * 1000);
+		})();
+	}
 });
--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -4,17 +4,14 @@
 
 <div id="fsWrapper" class="container-fluid">
 	<div class="content">
-		{{-- Insert images to process here --}}
 		<div class="fspolar">
+			{{-- Insert images to process here --}}
 			<img src="{{URL::asset('/image/1.jpg')}}">
 			<img src="{{URL::asset('/image/2.jpg')}}">
-		</div>
-{{-- 		<div class="fspolar">
-			<img src="{{URL::asset('/image/2.jpg')}}">
-		</div>
-		<div class="fspolar">
 			<img src="{{URL::asset('/image/3.jpg')}}">
-		</div> --}}
+			<img src="{{URL::asset('/image/4.jpg')}}">
+			<img src="{{URL::asset('/image/5.jpg')}}">
+		</div>
 	</div>
 </div>
 
