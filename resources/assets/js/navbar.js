a66a6f3f208a14d5567c618f5dbb12cf63a3952e
Austin Weidler <aweidler@warwicksd.org>
2017-10-01 17:03:22 -0400

    Made a few design tweaks

--- resources/assets/js/navbar.js
+++ resources/assets/js/navbar.js
@@ -2,11 +2,18 @@ $(function(){
 	
 	var $navlinks = $('#aw_navbar .navbar-links');
 	var $navlinksa = $('a', $navlinks);
+	var $active = $navlinks.find('li.active');
 
 	$navlinksa.hover(function(ein){
 		$navlinksa.not(this).addClass('blur');
+		if($active.length){
+			$active.removeClass('active');
+		}
 	}, function(eout){
 		$navlinksa.removeClass('blur');
+		if($active.length){
+			$active.addClass('active');
+		}
 	});
 
 });
