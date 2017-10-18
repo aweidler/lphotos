61b73b93cc9b4e7c1f4dd6ad76b4684ac3ab7a2c
Austin Weidler <aweidler@warwicksd.org>
2017-10-17 22:26:28 -0400

    Added back in commented stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,3 +30,15 @@ Route::post('/aupload/fsave/{id}', 'UploadController@saveFile');
 Route::post('/aupload/fsaveorder', 'UploadController@saveOrder');
 Route::post('/aupload', 'UploadController@store');
 Route::post('/aupload/{id}', 'UploadController@save');
+
+// images
+// Route::get('/img/{driver}/{name}', function($driver, $name){
+// 	ini_set('memory_limit','256M');
+// 	//$path = Storage::disk($driver)->getDriver()->getAdapter()->getPathPrefix().$name;
+// 	$data = Storage::disk($driver)->get($name);
+
+// 	$response = Image::make($data)->response();
+// 	$response->setLastModified(new DateTime("now"));
+// 	$response->setExpires(new DateTime("tomorrow"));
+// 	return $response;
+// });
